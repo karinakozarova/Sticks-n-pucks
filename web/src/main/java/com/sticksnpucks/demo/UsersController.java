75729971447e1eaf66c176fd45c4151bc070c208
Karina Kozarova <karinakozarova1303@gmail.com>
2020-10-09 09:11:03 +0200

    added a vue component for the demo

--- web/src/main/java/com/sticksnpucks/demo/UsersController.java
+++ web/src/main/java/com/sticksnpucks/demo/UsersController.java
@@ -6,23 +6,23 @@ import java.io.*;
 @RestController
 public class UsersController {
     @DeleteMapping("/user")
-     public int deleteAUser(@RequestParam(name="id", required=true) int id) {
+    public int deleteAUser(@RequestParam(name = "id", required = true) int id) {
         return ApiController.DeleteFromAPI(ApiController.GetBaseAPICallUrl() + "/account/" + id);
     }
 
     @PutMapping("/user/{id}")
-    public int updateAUser(@PathVariable (name="id", required=true) int id, @RequestParam(name="name", required=true) String name) {
+    public int updateAUser(@PathVariable(name = "id", required = true) int id, @RequestParam(name = "name", required = true) String name) {
         return ApiController.PutToAPI(ApiController.GetBaseAPICallUrl() + "/account/" + id + "/" + name);
     }
 
     @PostMapping("/user")
-    public int createAUser(@RequestParam(name="name", required=true) String name, @RequestParam(name="email", required=true) String email) {
+    public int createAUser(@RequestParam(name = "name", required = true) String name, @RequestParam(name = "email", required = true) String email) {
         return ApiController.PostToAPI(ApiController.GetBaseAPICallUrl() + "/account/" + name + "/" + email);
     }
 
     @GetMapping("/users")
-    public String getAUser(@RequestParam(name="id", required=false) Integer id) throws IOException {
-        if(id == null) {
+    public String getAUser(@RequestParam(name = "id", required = false) Integer id) throws IOException {
+        if (id == null) {
             return ApiController.GetAPIResult(ApiController.GetBaseAPICallUrl() + "/account");
         }
         return ApiController.GetAPIResult(ApiController.GetBaseAPICallUrl() + "/account/" + id);
