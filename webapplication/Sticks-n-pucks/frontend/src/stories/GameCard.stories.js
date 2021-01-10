eb2385667f2ec158891b4abbd05bc5db4e8ece87
Karina Kozarova <karinakozarova1303@gmail.com>
2021-01-10 22:39:46 +0100

    small fixes

--- webapplication/Sticks-n-pucks/frontend/src/stories/GameCard.stories.js
+++ webapplication/Sticks-n-pucks/frontend/src/stories/GameCard.stories.js
@@ -8,5 +8,5 @@ export const DefaultState = () => ({
     components: {
         card: GameCard
     },
-    template: `<card v-bind:hostName="'Hawks'" v-bind:guestName="'Best team'"  v-bind:hostScore="'1'" v-bind:guestScore="'2'" v-bind:gameDate="'2020-12-2'"/>`
+    template: `<card v-bind:hostname="'Hawks'" v-bind:guestname="'Best team'"  v-bind:hostscore="'1'" v-bind:guestscore="'2'" v-bind:gamedate="'2020-12-2'"/>`
 });
