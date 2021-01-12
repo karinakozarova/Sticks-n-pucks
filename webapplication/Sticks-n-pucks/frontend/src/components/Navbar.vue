d1fdf6a1777d964f51a5e19e513973ef8cd6ccd1
Karina Kozarova <karinakozarova1303@gmail.com>
2021-01-12 19:50:53 +0100

    updated the frontend

--- webapplication/Sticks-n-pucks/frontend/src/components/Navbar.vue
+++ webapplication/Sticks-n-pucks/frontend/src/components/Navbar.vue
@@ -16,10 +16,34 @@
                     </a>
                 </li>
                 <li class="my-2 md:my-0">
+                    <a href="/games/all"
+                       class="block py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
+                        <i class="fas fa-poll fa-fw mr-3"></i><span
+                            class="w-full inline-block pb-1 md:pb-0 text-sm">Games</span>
+                    </a>
+                </li>
+
+                <li class="my-2 md:my-0">
                     <a href="#"
                        class="block py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
-                        <i class="fas fa-chart-area fa-fw mr-3 text-indigo-400"></i><span
-                            class="w-full inline-block pb-1 md:pb-0 text-sm">Analytics</span>
+                        <i class="fas fa-chart-area fa-fw mr-3"></i><span
+                            class="w-full inline-block pb-1 md:pb-0 text-sm">My analytics</span>
+                    </a>
+                </li>
+
+                <li class="my-2 md:my-0">
+                    <a href="#"
+                       class="block py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
+                        <i class="fas fa-exclamation-triangle fa-fw mr-3"></i><span
+                            class="w-full inline-block pb-1 md:pb-0 text-sm">Penalties</span>
+                    </a>
+                </li>
+
+                <li class="my-2 md:my-0">
+                    <a href="/chat"
+                       class="block py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
+                        <i class="far fa-comments fa-fw mr-3"></i><span
+                            class="w-full inline-block pb-1 md:pb-0 text-sm">Chat</span>
                     </a>
                 </li>
             </ul>
