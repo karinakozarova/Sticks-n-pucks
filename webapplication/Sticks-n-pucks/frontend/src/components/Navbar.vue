6044c02af683d0d3010017a3ddc731851dcf0ba9
Karina Kozarova <karinakozarova1303@gmail.com>
2021-01-16 11:07:42 +0100

    updated the navabr

--- webapplication/Sticks-n-pucks/frontend/src/components/Navbar.vue
+++ webapplication/Sticks-n-pucks/frontend/src/components/Navbar.vue
@@ -40,6 +40,14 @@
                 </li>
 
                 <li class="my-2 md:my-0">
+                    <a href="/players/free-agents/all"
+                       class="block py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
+                        <i class="fas fa-user-friends fa-fw mr-3"></i><span
+                            class="w-full inline-block pb-1 md:pb-0 text-sm">Free agents</span>
+                    </a>
+                </li>
+
+                <li class="my-2 md:my-0">
                     <a href="/chat"
                        class="block py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
                         <i class="far fa-comments fa-fw mr-3"></i><span
